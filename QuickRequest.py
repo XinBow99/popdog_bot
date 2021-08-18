6fd9520f08467c575e5472b1ebb6bdace01c1290
Jason <asdewq45445@gmail.com>
2021-08-19 04:47:22 +0800

    optimized server switch

--- QuickRequest.py
+++ QuickRequest.py
@@ -1,28 +1,27 @@
 import requests
 import random
 users = [
-    ("github.com@XinBow99@request_dog", "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"),
-    ("tw-Xinbow99-1",                   "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"),
-    ("tw-Xinbow99-2",                   "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"),
-    ("tw-Xinbow99-3",                   "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"),
-    ("TW-KHC-Xinbow99-1",               "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"),
-    ("TW-KHC-Xinbow99-2",               "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"),
-    ("TW-KHC-Xinbow99-3",               "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"),
-    ("TW-KHC-Xinbow99-4",               "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"),
-    ("www.linkedin.com@in@Shiwen99",    "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"),
-    ("www.linkedin.com@in@Shiwen99",    "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"),
-    ("www.linkedin.com@in@Shiwen99",    "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"),
-    ("www.linkedin.com@in@Shiwen99",    "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"),
-    ("www.linkedin.com@in@Shiwen99",    "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"),
-    ("www.linkedin.com@in@Shiwen99",    "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"),
-    ("www.linkedin.com@in@Shiwen99",    "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"),
-    ("www.linkedin.com@in@Shiwen99",    "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"),
-    ("www.linkedin.com@in@Shiwen99",    "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"),
-    ("www.linkedin.com@in@Shiwen99",    "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx")
+    ("github.com@XinBow99@request_dog", "1234567-1234-4aa0-2133-jfwnefhjjfekw"),
+    ("tw-Xinbow99-1",                   "1234567-1234-5678-2133-jfwnefhjjfekw"),
+    ("tw-Xinbow99-2",                   "1234567-1234-5678-2133-jfwnefhjjfekw"),
+    ("tw-Xinbow99-3",                   "1234567-1234-5678-2133-jfwnefhjjfekw"),
+    ("TW-KHC-Xinbow99-1",               "1234567-1234-5678-2133-jfwnefhjjfekw"),
+    ("TW-KHC-Xinbow99-2",               "1234567-1234-5678-2133-jfwnefhjjfekw"),
+    ("TW-KHC-Xinbow99-3",               "1234567-1234-5678-2133-jfwnefhjjfekw"),
+    ("TW-KHC-Xinbow99-4",               "1234567-1234-5678-2133-jfwnefhjjfekw"),
+    ("www.linkedin.com@in@Shiwen99",    "1234567-1234-5678-2133-jfwnefhjjfekw"),
+    ("www.linkedin.com@in@Shiwen99",    "1234567-1234-5678-2133-jfwnefhjjfekw"),
+    ("www.linkedin.com@in@Shiwen99",    "1234567-1234-5678-2133-jfwnefhjjfekw"),
+    ("www.linkedin.com@in@Shiwen99",    "1234567-1234-5678-2133-jfwnefhjjfekw"),
+    ("www.linkedin.com@in@Shiwen99",    "1234567-1234-5678-2133-jfwnefhjjfekw"),
+    ("www.linkedin.com@in@Shiwen99",    "1234567-1234-5678-2133-jfwnefhjjfekw"),
+    ("www.linkedin.com@in@Shiwen99",    "1234567-1234-5678-2133-jfwnefhjjfekw"),
+    ("www.linkedin.com@in@Shiwen99",    "1234567-1234-5678-2133-jfwnefhjjfekw"),
+    ("www.linkedin.com@in@Shiwen99",    "1234567-1234-5678-2133-jfwnefhjjfekw"),
+    ("www.linkedin.com@in@Shiwen99",    "1234567-1234-5678-2133-jfwnefhjjfekw")
 ]
 for user in users:
     x = user
-    r = "http://{}:18116/trigger_path/create/{}/{}".format(
-        "127.0.0.1",x[0], x[1])
+    r = "http://{}:18117/trigger_path/create/{}/{}".format("127.0.0.1",x[0], x[1])
     res = requests.get(r)
     print(res.text)
--- request_dog.py
+++ request_dog.py
@@ -12,6 +12,9 @@ uuids = {
 
 }
 
+class DogError(Exception):
+    pass
+
 app = flask.Flask(__name__)
 CORS(app)
 if __name__ == "__main__":
@@ -50,18 +53,12 @@ def dog(user_name, uuid):
         # change username to your name
         "username": user_name,
         # change uuid to your uuid
-        "uuid": uuid
-    }
+        "uuid": uuid,
+	"token": "03AGdBq26TUjzUhg_4sEo96WvNQaEruUFWyPBopfqnx0b5RKKhCpuKqje4gJ1zsePRuIwYfpMLtMIlMgXlnFwZsYmoLZBuX6J5c91TXnV_nWU0ASyzAA-1QaHS_g3bK9y25i5lCMSpTWBcrRIqCyw494ycyNwCraRE4m2XXADiMrW5J4RmIkyaWRNug14tRcp6GzSWkvUHrp2Uo9CQAiE-sr20HoStgmuf4bctInqisMMWwbcTh-gcjEpgci4IMB58Po0x8Jqn2ZPlAiJnZsjfT14AT_3i9hjhMYkwJ5wLVzvSZKbRjSFPbQ-CS-PT9gsMa2J1eU7ORCBVG99OH_p92cfQtXQh7fAzeaWlmOd2R_V-hD6lvvYwGYgzjuGXxtR5jdWFRjhkMsmQpn4c0YoBMQ16HyB8DGRFQb7zehwmPhlm89PMp1Oga0XbNKNfbgl5M4l6esMtdcB-x-10t1_zSqCjMPBHB7dhtFMqDWcCdu41m_4xevF4S_3EgZ8RxOCh53Q71ylwUigK"}
     req_times = 0
     start_click = 0
     start_time = time.time()
-    while open('config.txt', 'r', encoding='utf-8').read().strip("\n") == "1":
-        uuids[uuid] = {
-            "bot_name": user_name,
-            "req_times": req_times,
-            "last_runtime": str(time.ctime())
-        }
-        headers = {
+    headers = {
             "Accept": "*/*",
             "Content-Type": "application/json;charset=utf-8",
             "Origin": "https://popdog.click",
@@ -72,37 +69,47 @@ def dog(user_name, uuid):
             "Accept-Encoding": "gzip, deflate, br",
             "Connection": "keep-alive",
         }
+    while open('config.txt', 'r', encoding='utf-8').read().strip("\n") == "1":
+        uuids[uuid] = {
+            "bot_name": user_name,
+            "req_times": req_times,
+            "last_runtime": str(time.ctime())
+        }
         try:
             res = requests.post(url, headers=headers, json=data, verify=False)
             req_times += 1
             if "Do not pet the Pop Dog too much" in res.text or res.status_code != 200:
-                raise "Pop Dog Request Error." + str(res.status_code)
+                raise DogError("Pop Dog Request Error." + str(res.status_code))
             else:
                 if start_click == 0:
                     start_click = int(res.json()['clicks'])
-                print("[Perfect] {} [Elapsed time] {:.2f}s\n[UUID] {}\n[Name] {}\n[Res] {}\n[Diff] {}\n{}".format(
+                print("[Perfect] {} [Elapsed time] {:.2f}s\n[UUID] {}\n[Name] {}\n[Res] {}\n[Diff] {}\n[x-ratelimit-remaining] {}\n{}".format(
                     req_times,time.time() - start_time,
                     uuid,
                     user_name,
                     res.text,
                     int(res.json()['clicks']) - start_click,
+                    res.headers['x-ratelimit-remaining'],
                     "----------------------------------------------------"
                 ))
                 uuids[uuid].update({
-                    'clicks': res.text
+                    'clicks': res.text,
+                    'x-ratelimit-remaining':res.headers['x-ratelimit-remaining']
                 })
+                if int(res.headers['x-ratelimit-remaining']) == 1:
+                    raise DogError("Change Server x-ratelimit-remaining is 1")
                 if req_times % 25 == 0:
                     time.sleep(15)
                 else:
                     time.sleep(7.5)
-        except:
+        except Exception as e:
             user_name = user_name.replace("/", "@")
             call_other_server(
                 call_server=app.config["call_server"],
                 user_name=user_name,
                 uuid=uuid,
                 trigger=app.config['trigger'],
-                error_type="Except Error"
+                error_type= str(e)
             )
             break
 
@@ -111,7 +118,6 @@ def call_other_server(call_server, user_name, uuid, trigger, error_type):
     global uuids
     print("[ERROR-Wait-For-10-Secs]", user_name, error_type,
           '\n-------------------------------------------')
-    time.sleep(7.5)
     print(requests.get(
         f"http://{call_server}:18116/{trigger}/create/{user_name}/{uuid}").text)
     uuids[uuid].update({
